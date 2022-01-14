b899065998b8cef9713f2080daa9314452f44c96
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-01-14 12:04:35 +0900

    商品出品の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(create_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:item).permit(:title, :text, :price, :category_id, :condition_id,:shipping_cost_id, :shipping_prefecture_id, :shipping_days_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
-  validates :title, :text, :price, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :shipping_cost, :shipping_prefecture_id, :shipping_days_id
+
 
   belongs_to :user
+  has_one_attached :image
+
+  validates :title, :text, :price, presence: true
+  validates :category_id, :condition_id, :shipping_cost_id,:shipping_prefecture_id, :shipping_days_id, numericality: { other_than: 1, message: "can't be blank" } 
+
 end
