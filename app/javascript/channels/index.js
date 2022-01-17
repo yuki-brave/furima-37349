a4811bea64298d168abde4409a8ae037f9d2acc9
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-01-17 14:10:59 +0900

    利益計算の実装

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,19 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+function calculation() {
+  const itemPrice = document.getElementById("item-price");
+  itemPrice = addEventListener("keyup", () =>{
+    const commission = Math.floor(itemPrice.value * 0.1);
+    const result = itemPrice.value - commission;
+
+    const calcTax = document.getElementById("add-tax-price");
+    const calcProfit = document.getElementById("profit");
+
+    calcTax.innerHTML = commission;
+    calcProfit.innerHTML = result;
+  });
+};
+  
+window,addEventListener('load', calculation)
