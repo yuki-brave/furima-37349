9b3a2b47c149674c019b209d1dcbb1bd9811595c
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-01-18 16:02:21 +0900

    条件分岐による画面遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.order('created_at DESC').includes(:user)
@@ -41,4 +42,12 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:title, :text, :price, :category_id, :condition_id, :shipping_cost_id, :shipping_prefecture_id,
                                  :shipping_days_id, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
 end
