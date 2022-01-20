f2e20522979b45a50ac76756f344ba34ac53ef83
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-01-20 18:45:52 +0900

    バリデーションの記述

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -2,7 +2,7 @@ class PurchaseDestination
   include ActiveModel::Model
   attr_accessor :post_code, :shipping_prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id, :token
   with_options presence: true do
-    validates :city, :address, :user_id, :item_id
+    validates :city, :address, :user_id, :item_id, :token
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone_number, format: {with: /\A[0-9]{11}\z/}
   end
