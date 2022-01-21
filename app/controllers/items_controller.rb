cc3150ff0d219badfe90bde435a0725fa9adc8df
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-01-21 16:20:14 +0900

    条件付き画面遷移の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,6 +51,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless current_user.id == @item.user_id || @item.purchase.blank?
+    redirect_to action: :index if current_user.id != @item.user_id || @item.purchase.present?
   end
 end
