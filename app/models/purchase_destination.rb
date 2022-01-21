32617d9c7d99354cde53ad4f1b6b52b9bdc83f96
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-01-21 15:37:41 +0900

    単体テストコードの記述

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -4,7 +4,7 @@ class PurchaseDestination
   with_options presence: true do
     validates :city, :address, :user_id, :item_id, :token
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :phone_number, format: {with: /\A[0-9]{11}\z/}
+    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/}
   end
   validates :shipping_prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
